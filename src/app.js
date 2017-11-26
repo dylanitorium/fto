412661dfebe513945a8475200063680797f7df02
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-11-26 16:17:49 +1300

    * Adding SafeAreaView wrapper for iPhone X

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { NativeRouter } from 'react-router-native';
 import { store, persistor } from './redux/store';
 import Routes from './routes';
 import Loader from './components/screens/Loader';
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  }
+})
 
 const App = () => (
   <Provider store={store}>
@@ -13,8 +20,9 @@ const App = () => (
       loading={<Loader />}
     >
       <NativeRouter>
-        <Routes />
-
+        <SafeAreaView style={styles.safeArea}>
+          <Routes />
+        </SafeAreaView>
       </NativeRouter>
     </PersistGate>
   </Provider>
