7d25b4cd16f1b3c53a4cef91504e34e4874169fc
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-11-27 20:15:42 +1300

    * Set up some constants

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,9 @@ export const ROUTE_MAP = {
   '/onboard/overhead': 'Overhead',
   '/onboard/deadlift': 'Deadlift',
   '/week/1': 'Week One',
+  '/week/2': 'Week Two',
+  '/week/3': 'Week Three',
+  '/week/4': 'Deload Week',
 };
 
 export const EXERCISES = {
@@ -20,3 +23,22 @@ export const WEEKS = {
   THREE: '3',
   FOUR: '4',
 };
+
+export const WARM_UP_REPS = [5, 5, 5];
+
+export const WARM_UP_MODIFIER = [0.4, 0.5, 0.6];
+
+export const SET_REPS = {
+  [WEEKS.ONE]: [5, 5, 5],
+  [WEEKS.TWO]: [3, 3, 3],
+  [WEEKS.THREE]: [5, 3, 1],
+  [WEEKS.FOUR]: [5, 5, 5],
+}
+
+export const SET_MODIFIER = {
+  [WEEKS.ONE]: [0.65, 0.75, 0.85],
+  [WEEKS.TWO]: [0.7, 0.8, 0.9],
+  [WEEKS.THREE]: [0.75, 0.85, 0.95],
+  [WEEKS.FOUR]: [0.6, 0.6, 0.6],
+}
+
