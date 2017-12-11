0a1f02a3fac0b5a2c685dceb865f1800ae0016fd
Dylan Sweetensen <dylansweetensen@gmail.com>
2017-12-11 19:26:25 +1300

    * removing old dep

--- src/components/pure/Sets/SetItem.js
+++ src/components/pure/Sets/SetItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { Text, Row, Icon } from '@shoutem/ui';
 
 const SetItem = props => (
